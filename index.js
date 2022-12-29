278297a6fbd989def45884d920f78502dd585561
João Pedro Oliveira <joliveira@realtec.com.br>
2022-12-29 19:53:30 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function JoaoPedro () {
+  return (
+    <View>
+      <Text>
+        Olá! Esse é meu primeiro componente.
+      </Text>
+
+    </View>
+  )
+}
+
+export default JoaoPedro;
